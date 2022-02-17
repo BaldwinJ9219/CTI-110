417d61c96789344b7c1f7aa6b8b07cd407c63495
BaldwinJ9219 <99194954+BaldwinJ9219@users.noreply.github.com>
2022-02-17 15:34:09 -0500

    Add files via upload
    P1HW2 first submission 02/17/2022

---
+++ P1HW2_BaldwinJustin.py
@@ -0,0 +1,17 @@
+# This assignment is to show basic use and knowledge of IDLE programming
+# 02/17/2022
+# CTI-110 P1HW2 - Pizza Order
+# Justin Baldwin
+#
+
+Num = input("How many students do you want to order pizza for?")
+Num_2 = (int(Num) * (3));
+Num_3 = (int(Num) // (2))
+
+print('---Pizza Order---')
+print('Number of Students:', Num)
+print('Pizza Slices Needed:', Num_2)
+print('Pizzas Needed:',  Num_3)
+print('--------------------')
+
+
